edd97fdec5d9739b08709e0080711abc715fa68a
Oleg Troian <sleite4r@gmail.com>
2024-10-03 18:13:16 +0200

    create media-queries

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// BURGER-BTTN
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.burger-btn');
+  const navUL = document.querySelector(".nav-menu");
+  const bars = document.querySelectorAll('.bar');
+
+  hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+    bars.forEach((element) => {
+      element.classList.toggle('active');
+    });
+  });
+
+  navUL.addEventListener('click', () => { 
+    navUL.classList.remove('show');
+    bars.forEach((element) => {
+      element.classList.remove('active');
+    });
+  })
+});
